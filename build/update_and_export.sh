fd805f5ea191ad7508b1befb34a2d7958937c536
twak <twakelly@gmail.com>
2017-10-26 15:07:50 +0100

    ...

---
+++ build/update_and_export.sh
@@ -0,0 +1,9 @@
+for r in jutils campskeleton siteplan chordatlas;
+   do 
+       git clone --branch bigsur_docker "https://github.com/twak/$r.git"; \
+       cd $r; \
+       mvn install package; \
+       cd ..; \
+done
+
+cp /tmp/chordatlas/target/*.jar /output
