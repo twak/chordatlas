7df49449163a8390effa80b5dab675f96a4920ce
twak <twakelly@gmail.com>
2017-10-26 16:08:55 +0100

    config hell

--- build/update_and_export.sh
+++ build/update_and_export.sh
@@ -3,8 +3,11 @@ for r in jutils campskeleton siteplan chordatlas;
    do 
        git clone --branch bigsur_docker "https://github.com/twak/$r.git"; \
        cd $r; \
-       mvn install package; \
+       mvn install; \
        cd ..; \
 done
+
+cd /tmp/chordatlas
+mvn assembly:single
 
 cp /tmp/chordatlas/target/*.jar /output
