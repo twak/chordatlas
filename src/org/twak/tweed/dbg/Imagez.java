5e156429e7428a27236ddd3eccabd54b7cec8b63
twak <twakelly@gmail.com>
2017-12-20 23:47:07 +0000

    jpg writer with quality...

--- src/org/twak/tweed/dbg/Imagez.java
+++ src/org/twak/tweed/dbg/Imagez.java
@@ -6,7 +6,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+import javax.imageio.stream.FileImageOutputStream;
 
 import org.twak.utils.Filez;
 
@@ -16,31 +21,47 @@ public class Imagez {
 
 		int width = 0;
 		int height = 0;
-		for (BufferedImage vi : images) {
-			width = Math.max(width, vi.getWidth());
+		for ( BufferedImage vi : images ) {
+			width = Math.max( width, vi.getWidth() );
 			height += vi.getHeight();
 		}
-		
-		if (width == 0 || height == 0)
+
+		if ( width == 0 || height == 0 )
 			return;
-		
+
 		BufferedImage out = new BufferedImage( width, height, BufferedImage.TYPE_3BYTE_BGR );
 		Graphics2D g = out.createGraphics();
-		
+
 		int y = 0;
-		for (BufferedImage vi : images) {
-			g.drawImage( vi, ( width - vi.getWidth() ) /2 , y, vi.getWidth(), vi.getHeight(), null );
+		for ( BufferedImage vi : images ) {
+			g.drawImage( vi, ( width - vi.getWidth() ) / 2, y, vi.getWidth(), vi.getHeight(), null );
 			y += vi.getHeight();
 		}
-		
-		
-		
+
 		try {
-			ImageIO.write( out,  Filez.getExtn( file.getName() ) , file );
+			ImageIO.write( out, Filez.getExtn( file.getName() ), file );
 		} catch ( IOException e ) {
 			e.printStackTrace();
 		}
-		
+
+	}
+
+	public static void writeJPG( BufferedImage rendered, float quality, File f ) {
+
+		try {
+			JPEGImageWriteParam jpegParams = new JPEGImageWriteParam( null );
+			jpegParams.setCompressionMode( ImageWriteParam.MODE_EXPLICIT );
+			jpegParams.setCompressionQuality( 1f );
+
+			ImageWriter writer = ImageIO.getImageWritersByFormatName( "jpeg" ).next();
+			ImageWriteParam param = writer.getDefaultWriteParam();
+			param.setCompressionMode( ImageWriteParam.MODE_EXPLICIT ); // Needed see javadoc
+			param.setCompressionQuality( quality ); // Highest quality
+			writer.setOutput( new FileImageOutputStream( f ) );
+			writer.write( null, new IIOImage( rendered, null, null ), jpegParams );
+		} catch ( Throwable th ) {
+			th.printStackTrace();
+		}
 	}
 
 }
