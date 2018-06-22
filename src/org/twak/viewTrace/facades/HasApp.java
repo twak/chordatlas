d49dd98109534ac9593a736780e3f6aa929f6445
twakelly@gmail.com <twakelly@gmail.com>
2018-06-22 15:03:51 +0100

    moar shiny

---
+++ src/org/twak/viewTrace/facades/HasApp.java
@@ -0,0 +1,17 @@
+package org.twak.viewTrace.facades;
+
+import java.util.List;
+
+import org.twak.viewTrace.franken.App;
+
+public interface HasApp {
+
+	static App get( HasApp roofApp ) {
+		try {
+			return (App) roofApp.getClass().getField( "app" ).get( roofApp );
+		} catch ( Throwable e ) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
