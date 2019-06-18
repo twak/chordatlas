f75cc566ac9af591104c9ce2675d9107d4e61cb8
twak <twakelly@gmail.com>
2019-06-18 19:11:38 +0100

    skirts for depth'd polygon faces

--- src/org/twak/mmg/media/GreebleMMG.java
+++ src/org/twak/mmg/media/GreebleMMG.java
@@ -1,5 +1,6 @@
 package org.twak.mmg.media;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -19,17 +20,24 @@ import org.twak.mmg.Node;
 import org.twak.mmg.functions.MiniFacadeImport;
 import org.twak.mmg.ui.MOgramEditor;
 import org.twak.utils.collections.Loop;
+import org.twak.utils.collections.Loopable;
 import org.twak.utils.collections.Loopz;
+import org.twak.utils.collections.MultiMap;
 import org.twak.utils.geom.DRectangle;
+import org.twak.utils.geom.HalfMesh2;
+import org.twak.utils.geom.HalfMeshez;
 import org.twak.utils.geom.LinearForm3D;
+import org.twak.utils.geom.HalfMesh2.Builder;
+import org.twak.utils.geom.HalfMesh2.HalfEdge;
+import org.twak.utils.geom.HalfMesh2.HalfFace;
 import org.twak.viewTrace.facades.CGAMini;
 import org.twak.viewTrace.facades.GreebleHelper;
 import org.twak.viewTrace.facades.GreebleHelper.LPoint2d;
 import org.twak.viewTrace.facades.GreebleHelper.LPoint3d;
 import org.twak.viewTrace.facades.MMeshBuilderCache;
+import org.twak.viewTrace.facades.MatMeshBuilder;
 import org.twak.viewTrace.facades.MiniFacade;
 import org.twak.viewTrace.facades.MiniFacade.Feature;
-
 import smile.math.Math;
 
 
@@ -51,25 +59,88 @@ public class GreebleMMG {
 			if ( c.function.getClass() == MiniFacadeImport.class )
 				( (MiniFacadeImport) c.function ).mf = mf;
 		
+		MultiMap<DepthColor, Loop<Point2d>> shapes = new MultiMap<>();
 		
 		for (Node n : mogram.evaluate( new MMG() ).findNodes()) 
 			if ( n.context.mo.renderData != null && n.result instanceof org.twak.mmg.prim.Face && !n.erased ) {
 				DepthColor dc = (DepthColor)n.context.mo.renderData;
 				if (dc.visible) 
-					createSurfacesWithDepth( dc, ((org.twak.mmg.prim.Face)n.result).getPoints().get( 0 ), mbs, to3d );
+					shapes.put(dc, ((org.twak.mmg.prim.Face)n.result).getPoints().get( 0 ) );
 			}
+		
+		HalfMesh2 mesh = tesselate(shapes);
+		createSurfacesWithDepth( mesh, mbs, to3d );
 		
 	}
 
+	private class DepthHF extends HalfFace {
+		
+		private DepthColor dc;
+		
+		public DepthHF() {}
+		public DepthHF(HalfEdge e) {
+			super(e);
+		}
+	}
+	
+	private static HalfMesh2 tesselate( MultiMap<DepthColor, Loop<Point2d>> shapes ) {
+		
+		Builder b = new Builder( DepthHF.class, HalfEdge.class );
+		
+		for (DepthColor dc : shapes.keySet())
+			for (Loop<Point2d> loop : shapes.get(dc)) {
+				
+				for (Loopable<Point2d> ll : loop.loopableIterator() ) {
+					b.newPoint( ll.get() );
+				}
+				DepthHF face = (DepthHF) b.newFace();
+				face.dc = dc;
+			}
+		
+		
+		HalfMeshez.splitMergeCoincident( b.mesh, 0.01 );
+		
+		return b.mesh;
+	}
 
-	private static void createSurfacesWithDepth( DepthColor dc, Loop<Point2d> geometry, MMeshBuilderCache mbs, Matrix4d to3d ) {
+	
+	// dictated not executed, twak
+	private static void createSurfacesWithDepth( HalfMesh2 mesh, MMeshBuilderCache mbs, Matrix4d to3d ) {
 		
-		if (geometry == null)
-			return;
+		for (HalfFace hf : mesh) {
 		
-		Loop <Point3d> p3 = Loopz.transform( Loopz.to3d( geometry, dc.depth, 1 ), to3d );
-		
-		mbs.get( UUID.randomUUID().toString(), dc.color, null ).add( p3.singleton(), null, false );
+			DepthColor dc = ((DepthHF)hf).dc;
+			
+			MatMeshBuilder mat = mbs.get( "mmg_"+dc.color.toString(), dc.color, null );
+			
+			// skirt around face
+			for (HalfEdge he : hf) {
+				
+				double heDepth = dc.depth, overDepth = 0;
+				
+				if (he.over != null) 
+					overDepth = ((DepthHF)he.over.face).dc.depth;
+				
+				if (heDepth > 0 || he.over == null) {
+					
+					Point3d 
+						a = new Point3d (he.start.x, he.start.y, heDepth ),
+						b = new Point3d (he.end.x, he.end.y, heDepth ),
+						c = new Point3d (he.end.x, he.end.y, overDepth ),
+						d = new Point3d (he.start.x, he.start.y, overDepth );
+							
+					
+						for (Point3d p : new Point3d[] {a,b,d,c}  ) 
+							to3d.transform( p );
+					
+					mat.add( a, b, c, d );
+				}
+			}
+			
+			// main polygon
+			Loop <Point3d> p3 = Loopz.transform( Loopz.to3d( hf.toLoop(), dc.depth, 1 ), to3d );
+			mat.add( p3.singleton(), null, false );
+		}
 	}
 	
 	public static MOgram createMOgram(MiniFacade mf) {
