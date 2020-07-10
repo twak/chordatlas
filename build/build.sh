7f7d813e4b69e77279baaa37349ed6ad3784c496
twakelly@gmail.com <twakelly@gmail.com>
2020-07-10 22:22:53 +0100

    no cache githube

---
+++ build/build.sh
@@ -0,0 +1,1 @@
+docker image build --tag chordatlas --file Dockerfile .
--- build/update_and_export.sh
+++ build/update_and_export.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-for r in jutils campskeleton siteplan chordatlas;
-   do 
-       cd /tmp/$r; \
-       git pull; \
-       mvn install; \
-done
+#for r in jutils campskeleton siteplan chordatlas;
+#   do
+#       cd /tmp/$r; \
+#       git pull; \
+#       mvn install; \
+#done
 
 cd /tmp/chordatlas
-mvn package
+mvn package -T 64
 
 mkdir -p /output
-cp /tmp/chordatlas/target/chordatlas-0.0.1-SNAPSHOT.jar /output
+cp /tmp/chordatlas/target/chordatlas-0.0.1.jar /output
